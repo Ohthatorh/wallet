8f8cf2d4aaf9fc45652a87aa810494ce85b35944
unknown <ohthatorh@yandex.ru>
2022-04-27 10:08:13 +0300

    upd 27.04

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-//eslint-disable-next-line  no-undef
+//eslint-disable-next-line no-undef
 const path = require("path");
-//eslint-disable-next-line  no-undef
+//eslint-disable-next-line no-undef
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//eslint-disable-next-line  no-undef
+//eslint-disable-next-line no-undef
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-//eslint-disable-next-line  no-undef
+//eslint-disable-next-line no-undef
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+//eslint-disable-next-line no-undef
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === "development";
 const IS_PROD = NODE_ENV === "production";
@@ -17,21 +19,17 @@ function setupDevtool() {
     return false;
   }
 }
-//eslint-disable-next-line  no-undef
+//eslint-disable-next-line no-undef
 module.exports = {
   entry: "./src/script.js",
   output: {
-    //eslint-disable-next-line  no-undef
+    //eslint-disable-next-line no-undef
     path: path.resolve(__dirname, "./dist"),
     filename: "main.js",
     publicPath: "/",
   },
   devServer: {
     historyApiFallback: true,
-    static: {
-      //eslint-disable-next-line  no-undef
-      directory: path.join(__dirname, "./src"),
-    },
   },
   module: {
     rules: [
@@ -41,36 +39,29 @@ module.exports = {
         exclude: "/node_modules",
       },
       {
-        test: /\.module\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[local]__[sha1:hash:hex:7]",
-              },
-            },
-          },
-        ],
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
       {
-        test: /^((?!\.module).)*css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.(?:ico|gif|png|jpg|jpeg|avif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
   devtool: setupDevtool(),
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "main.css",
+      filename: "index.css",
     }),
     new HtmlWebpackPlugin({
-      title: "Wallet",
-      //eslint-disable-next-line  no-undef
-      inject: true,
-      template: "src/index.html",
+      //eslint-disable-next-line no-undef
+      template: path.resolve(__dirname, "./src/index.html"),
       filename: "index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
